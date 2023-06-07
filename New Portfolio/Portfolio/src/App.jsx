91e31e7f78d7c380af43502cbff4996afee4f817
Jeremy <jezzalittle20@gmail.com>
2023-06-07 12:14:47 +1000

    Fixing the file structure

---
+++ New Portfolio/Portfolio/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Intro from './components/intro'
+import Portfolio from './components/Porfolio'
+import Timeline from './components/Timeline'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div className='App'>
+      <Intro />
+      <Portfolio />
+      <Timeline />
+      <Contact />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ New Portfolio/Portfolio/src/components/Intro.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Intro() {
+    return (
+        <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
+            <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">Jeremy Little</h1>
+            <p className="text-base md:text-xl mb-3 font-medium">testing</p>
+            <p className="text-base md:text-xl mb-3 font-medium">testing2</p>
+        </div>
+    )
+}
+
+export default Intro
