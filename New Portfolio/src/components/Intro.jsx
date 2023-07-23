acc117ff16257fdc990b4dd7880effd087c2f5a2
Jeremy <jezzalittle20@gmail.com>
2023-07-23 16:01:41 +1000

    Fixed Tailwind

--- New Portfolio/src/components/Intro.jsx
+++ New Portfolio/src/components/Intro.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function Intro() {
     return (
         <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
-            <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">Jeremy Little</h1>
-            <p className="text-base md:text-xl mb-3 font-medium">testing</p>
+            <h1 className="text-4xl text-center md:text-7xl mb-1 md:mb-3 font-bold">Jeremy Little</h1>
+            <p className="text-base md:text-xl mb-3 font-medium"> </p>
             <p className="text-base md:text-xl mb-3 font-medium">testing2</p>
         </div>
     )
