1cde829e9d3e31d97ed8978f1087c08354e36d9d
Jeremy <jezzalittle20@gmail.com>
2024-03-13 13:26:49 +1100

    Updating the deploy github action

--- New Portfolio/vite.config.ts
+++ New Portfolio/vite.config.ts
@@ -3,11 +3,20 @@ import path from "path"
 import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => {
+  const config = {
+
   plugins: [react()],
+  base: '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+}
+  if (command !== 'serve') {
+    config.base = '/Portfolio/'
+  }
+
+  return config
 })
